2e8426d644f08e488cc646230987ff9b1d6ae054
liuye03 <liuye03@meituan.com>
2016-07-04 20:13:43 +0800

    add webp demo

--- demo/routes/index.js
+++ demo/routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'export' });
 });
 
+router.get('/webp', function(req, res, next) {
+  res.render('webp', { title: 'export' });
+});
+
 module.exports = router;
