99a475d5cdb0a06f7e112099034154c768432504
liuye03 <liuye03@meituan.com>
2016-07-10 22:30:00 +0800

    add redis demo

--- demo/routes/index.js
+++ demo/routes/index.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var router = express.Router();
+var redis = require('redis'),
+    client = redis.createClient();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -8,6 +10,31 @@ router.get('/', function(req, res, next) {
 
 router.get('/webp', function(req, res, next) {
   res.render('webp', { title: 'export' });
+});
+
+router.get('/redis', function(req, res, next) {
+
+  var redis_data=[];
+  client.on('error',function(err) {
+  		console.log(err);
+  });	
+
+  client.set("string key", "string val", redis.print);
+
+  client.hset("node key","node redis1",'value',redis.print);
+  client.hset("node key","node redis2",'value',redis.print);
+  client.hkeys("node key", function (err, replies) {
+    console.log(replies.length + " replies:");
+    replies.forEach(function (reply, i) {
+        console.log("    " + i + ": " + reply);
+        redis_data[i] = reply; 
+    });
+    // client.quit();
+    console.log("redis data："+redis_data);
+    res.render('redis', { title: 'redis' , data: redis_data});
+	});
+
+  
 });
 
 module.exports = router;
