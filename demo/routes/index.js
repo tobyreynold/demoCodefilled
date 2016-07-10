73557dcffcf91692c5c75b0d7267b2bf0a6a50c7
liuye03 <liuye03@meituan.com>
2016-07-10 22:37:18 +0800

    remove

--- demo/routes/index.js
+++ demo/routes/index.js
@@ -14,25 +14,25 @@ router.get('/webp', function(req, res, next) {
 
 router.get('/redis', function(req, res, next) {
 
-  var redis_data=[];
-  client.on('error',function(err) {
-  		console.log(err);
-  });	
+ //  var redis_data=[];
+ //  client.on('error',function(err) {
+ //  		console.log(err);
+ //  });	
 
-  client.set("string key", "string val", redis.print);
+ //  client.set("string key", "string val", redis.print);
 
-  client.hset("node key","node redis1",'value',redis.print);
-  client.hset("node key","node redis2",'value',redis.print);
-  client.hkeys("node key", function (err, replies) {
-    console.log(replies.length + " replies:");
-    replies.forEach(function (reply, i) {
-        console.log("    " + i + ": " + reply);
-        redis_data[i] = reply; 
-    });
-    // client.quit();
-    console.log("redis data："+redis_data);
-    res.render('redis', { title: 'redis' , data: redis_data});
-	});
+ //  client.hset("node key","node redis1",'value',redis.print);
+ //  client.hset("node key","node redis2",'value',redis.print);
+ //  client.hkeys("node key", function (err, replies) {
+ //    console.log(replies.length + " replies:");
+ //    replies.forEach(function (reply, i) {
+ //        console.log("    " + i + ": " + reply);
+ //        redis_data[i] = reply; 
+ //    });
+ //    // client.quit();
+ //    console.log("redis data："+redis_data);
+ //    res.render('redis', { title: 'redis' , data: redis_data});
+	// });
 
   
 });
